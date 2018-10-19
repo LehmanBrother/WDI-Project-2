c4e3590675b7b44310332b0f7b043347e0cd0549
LehmanBrother <chrislehman1991@gmail.com>
2018-10-19 15:50:46 -0500

    New page and additional CSS

---
+++ controllers/question.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Question = require('../models/question');
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
+
+const Article = require('./models/article');
+const Comment = require('./models/comment');
+const Question = require('./models/question');
+const User = require('./models/user');
+
+const articleController = require('./controllers/article');
+const commentController = require('./controllers/comment');
+const questionController = require('./controllers/question');
+const userController = require('./controllers/user');
+
 require('./db/db');
 
 //Middleware
@@ -16,10 +27,27 @@ app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
 //Controllers
+// app.use('/articles', articleController);
+// app.use('/comments', commentController);
+app.use('/questions', questionController);
+// app.use('/users', userController);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
+//home/index route
+app.get('/', async (req, res) => {
+	try {
+		const allQuestions = await Question.find();
+		res.render('index.ejs', {
+			questions: allQuestions
+		})
+	} catch(err) {
+		res.send(err);
+	}
 });
+
+//new question route
+app.get('/questions/new', async (req, res) => {
+	res.render('questions/new.ejs');
+})
 
 app.listen(3000, () => {
 	console.log('Server listening on port 3000');
